93065dd49d3a3bf2cee3e979a267e26422889123
rafal92909 <rafal92909@onet.eu>
2017-04-05 23:04:03 +0200

    views

---
+++ src/app/shared/ingrediend.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // } // jedna z opcji uzycia konstruktora, ale dość długa
+
+    constructor(public name: string, public amount: number) {} // skrócona wersja tego co powyżej
+}
